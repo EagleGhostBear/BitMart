7404121a335d1782565de4d58b7e5fc85a02289a
JIHYE <jye7921@gmail.com>
2023-06-21 18:47:56 +0900

    리뷰페이지 추가

--- src/main/webapp/src/pages/page.js
+++ src/main/webapp/src/pages/page.js
@@ -18,5 +18,6 @@ import Category from "./Category";
 import Order from "./Order";
 import Address from "./Address";
 import ConfirmPwd from "./ConfirmPwd";
+import Review from "./Review";
 
-export { Main, Login, Signup, CartList, CommentWrite, Detail, NotFound, Category, FindPwd, FindId, Order, Address, NoticeListPage, NoticeDetailPage, FaqPage, InquiryPage, InquiryFormPage, Payment, OrderList, ConfirmPwd };
+export { Main, Login, Signup, CartList, CommentWrite, Detail, NotFound, Category, FindPwd, FindId, Order, Address, NoticeListPage, NoticeDetailPage, FaqPage, InquiryPage, InquiryFormPage, Payment, OrderList, ConfirmPwd, Review };
--- src/main/webapp/src/shared/App.js
+++ src/main/webapp/src/shared/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { actionCreators as userActions } from '../redux/modules/user';
-import '../App.css';
+import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { actionCreators as userActions } from "../redux/modules/user";
+import "../App.css";
 
 // Components
-import { Header } from '../components/component';
+import { Header } from "../components/component";
 
 // Pages
 import {
@@ -31,21 +31,22 @@ import {
   FaqPage,
   InquiryPage,
   InquiryFormPage,
+  Review,
 } from "../pages/page";
 
 function App() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const token_key = localStorage.getItem('token');
+  const token_key = localStorage.getItem("token");
   const islogin = useSelector((state) => state.user.is_login);
   console.log("islogin: ", islogin);
   const [searchValue, setSearchValue] = useState(""); // 검색어 상태
-  const [selectedTag, setSelectedTag] = useState('');
+  const [selectedTag, setSelectedTag] = useState("");
 
   const handleSearchSubmit = (value) => {
-    console.log('검색어: ' + value);
+    console.log("검색어: " + value);
     setSearchValue(value);
-    navigate('/category');
+    navigate("/category");
   };
 
   useEffect(() => {
@@ -55,7 +56,7 @@ function App() {
     if (token_key !== null && token_key !== "null") {
       console.log("토큰 있음:" + token_key);
       dispatch(userActions.loginCheckDB(token_key));
-    }    
+    }
   }, []);
 
   return (
@@ -70,7 +71,10 @@ function App() {
         <Route path="/comment/write/:id" element={<CommentWrite />} />
         <Route path="/order" element={<Order />} />
         <Route path="/address" element={<Address />} />
-        <Route path="/category/:tag" element={<Category searchValue={searchValue} />} />
+        <Route
+          path="/category/:tag"
+          element={<Category searchValue={searchValue} />}
+        />
         <Route path="/FindPwd" element={<FindPwd />} />
         <Route path="/FindId" element={<FindId />} />
         <Route path="/confirmPwd" element={<ConfirmPwd />} />
@@ -82,6 +86,7 @@ function App() {
         <Route path="/inquiry" element={<InquiryPage />} />
         <Route path="/inquiry/wri" element={<InquiryFormPage />} />
         <Route path="/*" element={<NotFound />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
     </div>
   );
