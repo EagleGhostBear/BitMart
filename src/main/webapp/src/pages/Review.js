60d4a4c20bf0fd61520b5deb16e6afecb6800732
KIMDONGBEEN <dd1761@naver.com>
2023-06-22 19:33:36 +0900

    23-06-22

--- src/main/webapp/src/pages/Review.js
+++ src/main/webapp/src/pages/Review.js
@@ -62,16 +62,18 @@ const Mypage = () => {
             <a className={styles["menu-a"]}>
               주문내역
               <svg
-                className={styles["menu-a-svg"]}
-                height="19"
-                width="19"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  id="gfk9q0rhta"
-                  d="M1.657 1.657L9.657 1.57 9.657 9.657"
-                ></path>
+                      id="Arrow"
+                      height="19"
+                      width="19"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <defs>
+                        <path
+                          id="gfk9q0rhta"
+                          d="M1.657 1.657L9.657 1.657 9.657 9.657"
+                        />
+                      </defs>
                 <g fill="none" fillRule="evenodd">
                   <g>
                     <g>
