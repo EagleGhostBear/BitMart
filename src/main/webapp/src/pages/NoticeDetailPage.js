1808f3bccad28acb6be84b37b96c7dd8c3139dd3
leesohun <leesohun99@gmail.com>
2023-06-26 18:16:10 +0900

    css수정

--- src/main/webapp/src/pages/NoticeDetailPage.js
+++ src/main/webapp/src/pages/NoticeDetailPage.js
@@ -3,8 +3,8 @@ import NoticeDetail from '../components/NoticeDetail';
 
 const NoticeDetailPage = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', textAlign: 'center', marginTop: '-200px' }}>
-      <h1 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>공지사항</h1>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', textAlign: 'center', marginTop: '-200px', minHeight: '2000px' }}>
+      <h1 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}></h1>
       <NoticeDetail />
     </div>
   );
